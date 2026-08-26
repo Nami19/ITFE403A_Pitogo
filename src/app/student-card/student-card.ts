14a64b5e4fec967ee27f3ec1401634805a780287
Nami19 <bonnamae.pitogo@dbtc-cebu.edu.ph>
2026-08-26 11:14:17 +0800

    Activity: Conditional UI

--- src/app/student-card/student-card.ts
+++ src/app/student-card/student-card.ts
@@ -1,55 +1,67 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-student-card',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './student-card.html',
   styleUrl: './student-card.css'
 })
-export class StudentCard {
+export class StudentCard implements OnInit {
+
+  loading: boolean = true;
 
   students = [
     {
       name: 'Jownie Ann D. Malate',
       course: 'BSIT',
       year: '4th Year',
-      isFavorite: false
+      isFavorite: false,
+      active: true
     },
     {
       name: 'Mary Beth D. Gracia',
       course: 'BSIT',
       year: '4th Year',
-      isFavorite: false
+      isFavorite: false,
+      active: false
     },
     {
       name: 'Kurt Wojtyle S. Rizal',
       course: 'BSCS',
       year: '1st Year',
-      isFavorite: false
+      isFavorite: false,
+      active: true
     },
     {
       name: 'Niel Jhon E. Celocia',
       course: 'BSIT',
       year: '4th Year',
-      isFavorite: false
+      isFavorite: false,
+      active: false
     }
   ];
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 3);
+  }
+
   favoriteClicked(student: any) {
-  student.isFavorite = !student.isFavorite;
-  console.log(`${student.name} favorite status:`, student.isFavorite);
-}
+    student.isFavorite = !student.isFavorite;
+    console.log(`${student.name} favorite status:`, student.isFavorite);
+  }
 
   deleteStudent(studentName: string) {
-  this.students = this.students.filter(student => student.name !== studentName);
-  console.log('Deleted student:', studentName);
-}
+    this.students = this.students.filter(student => student.name !== studentName);
+    console.log('Deleted student:', studentName);
+  }
 
   editClicked(studentName: string) {
-  console.log('Editing', studentName);
-  alert('Editing ' + studentName);
-}
+    console.log('Editing', studentName);
+    alert('Editing ' + studentName);
+  }
 
 }
